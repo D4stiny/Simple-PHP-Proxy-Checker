a9e9ae7022a3634e48128f4d3c60c2a069b5b0ab
D4stiny <theteched33+mee@gmail.com>
2016-08-08 22:33:57 -0400

    Updated comments
    Updated comments for an easier understanding.

--- proxycheck.php
+++ proxycheck.php
@@ -1,14 +1,15 @@
 <?php
 
+// Simple PHP Proxy Check
+
+// CONFIGURATION
+$emailaddress = ""; // Your email address is required for the getipintel.net api
+$enableprobing = 1; // Probing involves the server to ping the clients IP to check for known open proxy ports - May slow load times
+
 // Functions for later use
 function isProxy() {
 	die("Proxy detected!");
 }
-
-// Proxy Detection Module
-
-$emailaddress = ""; // Your email address is required for the getipintel.net api
-$enableprobing = 1; // Probing involves the server to ping the clients IP to check for known open proxy ports - May slow load times
 
 // Step 1: Check IP through proxy databases
 
@@ -23,6 +24,8 @@ if($emailaddress != "") {
 		isProxy();
 	}
 }
+
+// Probe the client IP address (Can be disabled by changing $enableprobing to 0)
 
 if($enableprobing == 1) {
 	$knownports = array(80,81,553,554,1080,3128,4480,6588,8000,8080); // Known proxy ports
